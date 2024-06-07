1bdc856d9944209d1127849f7058be30c65f07bd
Shish-ai-ai <ivantaran48@gmail.com>
2024-06-07 14:43:14 +0300

    fix

--- PiIT_BE/urls.py
+++ PiIT_BE/urls.py
@@ -34,7 +34,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authorization.urls')),
-    path('news/', include('news.urls')),
+    path('api/news/', include('news.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
